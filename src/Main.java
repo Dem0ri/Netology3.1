c5632b04d88bba4173ca4c5ccc903413b84f8371
Dem0ri <odo34mai.ru@mail.ru>
2026-01-28 23:50:39 +0300

    Добавил ввод цены билета

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int price = 10_000;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите стоимость билета в рублях: ");
+        int price = scanner.nextInt();
         int miles = service.calculate(price); // должно получиться 500
         System.out.println(miles);
     }
